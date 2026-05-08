44a06cde254de2852f5b79a56b4eeab75491bf01
Ficapro543 <ficapro543@gmail.com>
2026-05-07 22:34:40 -0500

    Se agregan mensajes de validación y confirmación

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ function agregarContacto() {
     let telefono = document.getElementById("telefono").value;
     let correo = document.getElementById("correo").value;
 
+    let mensaje = document.getElementById("mensaje");
+
+    // Validación de campos vacíos
+    if (nombre === "" || telefono === "" || correo === "") {
+
+        mensaje.innerText =
+        "Todos los campos son obligatorios.";
+
+        mensaje.style.color = "red";
+
+        return;
+    }
+
     let lista = document.getElementById("listaContactos");
 
     let contacto = document.createElement("div");
-    
+
     contacto.classList.add("contacto");
+
     contacto.innerHTML =
         "<h3>" + nombre + "</h3>" +
         "<p>" + telefono + "</p>" +
@@ -17,6 +31,13 @@ function agregarContacto() {
 
     lista.appendChild(contacto);
 
+    // Mensaje de confirmación
+    mensaje.innerText =
+    "Contacto agregado correctamente.";
+
+    mensaje.style.color = "green";
+
+    // Limpiar inputs
     document.getElementById("nombre").value = "";
     document.getElementById("telefono").value = "";
     document.getElementById("correo").value = "";
