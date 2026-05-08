08cb9c518e46aec44aaf7523a84838ed10ed1ee8
Ficapro543 <ficapro543@gmail.com>
2026-05-07 22:18:56 -0500

    Se implementa funcionalidad para agregar contactos

---
+++ script.js
@@ -0,0 +1,21 @@
+function agregarContacto() {
+
+    let nombre = document.getElementById("nombre").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+
+    let lista = document.getElementById("listaContactos");
+
+    let contacto = document.createElement("div");
+
+    contacto.innerHTML =
+        "<h3>" + nombre + "</h3>" +
+        "<p>" + telefono + "</p>" +
+        "<p>" + correo + "</p>";
+
+    lista.appendChild(contacto);
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("correo").value = "";
+}
